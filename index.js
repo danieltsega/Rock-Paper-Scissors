c79b9e2cbce26f07711763e602f3b8bf9b206a74
DannySanchez6658 <danieltsega6658@gmail.com>
2023-03-20 13:14:28 +0300

    [Fix]: Fixed some issues on how the player can keep track of its score

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
+let user = 0;
+let computer = 0;
+
 // The function that lets the user to play one round by computing the insertion of both players
 function playRound(playerSelection, computerSelection) {
   //check for the player having 'Rock'
   if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-    console.log("Yay! I won Rock beats Scissors");
+    //console.log("Yay! I won Rock beats Scissors");
+    user += 1;
+    return user;
   } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-    console.log("You Lose! Paper beats Rock");
+    //console.log("You Lose! Paper beats Rock");
+    computer += 1;
+    return computer;
   } else if (playerSelection === "ROCK" && computerSelection === "ROCK") {
-    console.log("Opps Draw!!");
+    //console.log("Opps Draw!!");
+    return user, computer;
   }
   //check for the player having 'Scissors'
   else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
-    console.log("Opps Draw!!");
+    //console.log("Opps Draw!!");
+    return user, computer;
   } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-    console.log("Yay! I won Scissor beats Paper");
+    //console.log("Yay! I won Scissor beats Paper");
+    user += 1;
+    return user;
   } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-    console.log("You Lose! Rock beats Scissors");
+    //console.log("You Lose! Rock beats Scissors");
+    computer += 1;
+    return computer;
   }
   //check for the player having 'Paper'
   else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
-    console.log("Opps Draw!!");
+    //console.log("Opps Draw!!");
+    return user, computer;
   } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-    console.log("Yay! I won PAPER beats ROCK");
+    //console.log("Yay! I won PAPER beats ROCK");
+    user += 1;
+    return user;
   } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-    console.log("Yay! I won PAPER beats ROCK");
+    //console.log("! I won PAPER beats ROCK");
+    computer += 1;
+    return computer;
   }
 }
 
@@ -40,15 +58,19 @@ function getPlayerSelection() {
 //------------------Creating a Game function that lets user to play up to 5 rounds---------------
 
 function game() {
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerSelection();
+    const computerSelection = getComputerChoice();
     playRound(playerSelection, computerSelection);
+  }
+  if (user > computer) {
+    console.log("Yay You Won By score of: ", user);
+  } else if (user > computer) {
+    console.log("Opps Computer Won By score of: ", computer);
   }
 }
 
 // Declaring both the computer function and also the player selection of the game
-const playerSelection = getPlayerSelection();
-const computerSelection = getComputerChoice();
-console.log("Computer: ", computerSelection);
-console.log("You: ", playerSelection);
+
 game();
 //playRound(playerSelection, computerSelection);
