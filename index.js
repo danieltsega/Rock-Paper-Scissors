1ada96ba7dfa63ac2fb7e7d3bac2a101efa9fbdf
danieltsega <danieltsega6658@gmail.com>
2024-08-08 17:15:12 +0300

    fix: fix the code to add playround to work and add score setting way

--- index.js
+++ index.js
@@ -3,116 +3,130 @@ let computer = 0;
 let roundUser = 0;
 let roundComp = 0;
 
+let compScore = document.getElementById("compScore");
+let userScore = document.getElementById("userScore");
+compScore.textContent = "0";
+currentScoreComp = 0;
+userScore.textContent = "0";
+currentScoreUser = 0;
+
+let statusDiv = document.getElementById("status");
+
 // The function that lets the user to play one round by computing the insertion of both players
 function playRound(playerSelection, computerSelection) {
   //check for the player having 'Rock'
-  if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+  if (playerSelection === "rock" && computerSelection === "scissor") {
     //console.log("Yay! I won Rock beats Scissors");
+    statusDiv.innerHTML = "";
     user += 1;
     roundUser += 1;
+
+    currentScoreUser += 1;
+    userScore.textContent = currentScoreUser;
+    statusDiv.textContent = "You Won!";
     return user, roundUser;
-  } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
     //console.log("You Lose! Paper beats Rock");
+    statusDiv.innerHTML = "";
     computer += 1;
     roundComp += 1;
+
+    currentScoreComp += 1;
+    compScore.textContent = currentScoreComp;
+    statusDiv.textContent = "You Lost!";
     return computer, roundComp;
-  } else if (playerSelection === "ROCK" && computerSelection === "ROCK") {
+  } else if (playerSelection === "rock" && computerSelection === "rock") {
     //console.log("Opps Draw!!");
+    statusDiv.innerHTML = "";
     user = user;
     computer = computer;
     roundComp = roundComp;
     roundUser = roundUser;
+
+    statusDiv.textContent = "Draw!";
     return user, computer, roundComp, roundUser;
   }
   //check for the player having 'Scissors'
-  else if (playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
+  else if (playerSelection === "scissor" && computerSelection === "scissor") {
     //console.log("Opps Draw!!");
+    statusDiv.innerHTML = "";
     user = user;
     computer = computer;
     roundComp = roundComp;
     roundUser = roundUser;
+
+    statusDiv.textContent = "Draw!";
     return user, computer, roundComp, roundUser;
-  } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+  } else if (playerSelection === "scissor" && computerSelection === "paper") {
     //console.log("Yay! I won Scissor beats Paper");
+    statusDiv.innerHTML = "";
     user += 1;
     roundUser += 1;
+
+    currentScoreUser += 1;
+    userScore.textContent = currentScoreUser;
+    statusDiv.textContent = "You Won!";
     return user, roundUser;
-  } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+  } else if (playerSelection === "scissor" && computerSelection === "rock") {
     //console.log("You Lose! Rock beats Scissors");
+    statusDiv.innerHTML = "";
     computer += 1;
     roundComp += 1;
+
+    currentScoreComp += 1;
+    compScore.textContent = currentScoreComp;
+    statusDiv.textContent = "You Lost!";
     return computer, roundComp;
   }
   //check for the player having 'Paper'
-  else if (playerSelection === "PAPER" && computerSelection === "PAPER") {
+  else if (playerSelection === "paper" && computerSelection === "paper") {
     //console.log("Opps Draw!!");
+    statusDiv.innerHTML = "";
     user = user;
     computer = computer;
     roundComp = roundComp;
     roundUser = roundUser;
+
+    statusDiv.textContent = "Draw!";
     return user, computer, roundComp, roundUser;
-  } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
     //console.log("Yay! I won PAPER beats ROCK");
+    statusDiv.innerHTML = "";
     user += 1;
     roundUser += 1;
+
+    currentScoreUser += 1;
+    userScore.textContent = currentScoreUser;
+    statusDiv.textContent = "You Won!";
     return user, roundUser;
-  } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+  } else if (playerSelection === "paper" && computerSelection === "scissor") {
     //console.log("! I won PAPER beats ROCK");
+    statusDiv.innerHTML = "";
     computer += 1;
     roundComp += 1;
+
+    currentScoreComp += 1;
+    compScore.textContent = currentScoreComp;
+    statusDiv.textContent = "You Lost!";
     return computer, roundComp;
   }
 }
 
 //The function that generates the computer selection
 function getComputerChoice() {
-  let choices = ["ROCK", "PAPER", "SCISSORS"];
+  let choices = ["rock", "paper", "scissor"];
   let rand = Math.floor(Math.random() * choices.length);
+
+  let compDiv = document.getElementById("compDiv");
+  let imageCreate = document.createElement("img");
+  imageCreate.setAttribute("src", `images/${choices[rand]}.png`);
+  imageCreate.setAttribute("width", 50);
+  imageCreate.setAttribute("height", 50);
+
+  compDiv.innerHTML = "";
+  compDiv.append(imageCreate);
   return choices[rand];
 }
-// Creating a function that accepts users input
-function getPlayerSelection() {
-  //let choice = prompt("Enter Your Choice: ");
-  return choice.toUpperCase();
-}
-//------------------Creating a Game function that lets user to play up to 5 rounds---------------
-
-function game() {
-  for (let i = 0; i < 2; i++) {
-    const playerSelection = getPlayerSelection();
-    const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-    console.log(
-      `User Selected: ${playerSelection} Computer Selection: ${computerSelection}`
-    );
-    console.log(`User Score: ${user} Computer Score: ${computer}`);
-    if (roundUser > roundComp) {
-      console.log("Yay You Won By score of: ", roundUser);
-      roundUser -= 1;
-    } else if (roundUser < roundComp) {
-      console.log("Opps Computer Won By score of: ", roundComp);
-      roundComp -= 1;
-    } else if (roundUser == roundComp) {
-      console.log("DRAW!!");
-      roundUser -= 1;
-      roundComp -= 1;
-    }
-  }
-  if (user > computer) {
-    console.log(`Yay! You Won By Overall Score of: ${user} to ${computer} `);
-  } else if (computer > user) {
-    console.log(
-      `Opps Computer Won By Overall Score of: ${computer} to ${user}`
-    );
-  } else if (computer == user) {
-    console.log(`DRAW! In Score of ${user} to ${computer}`);
-  }
-}
-
-// Declaring both the computer function and also the player selection of the game
-
-//game();
-//playRound(playerSelection, computerSelection);
 
 function buttonClick(value) {
   let userDiv = document.getElementById("userDiv");
@@ -124,5 +138,6 @@ function buttonClick(value) {
   userDiv.innerHTML = "";
 
   userDiv.append(imageCreate);
-  console.log(value);
+
+  playRound(value, getComputerChoice());
 }
